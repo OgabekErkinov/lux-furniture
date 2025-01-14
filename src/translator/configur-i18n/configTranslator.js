e8015f6a55034142a077128eb2c4d4e1baeceb9a
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-01-14 09:56:53 +0500

    clone of chinar.uz is creating..

---
+++ src/translator/configur-i18n/configTranslator.js
@@ -0,0 +1,24 @@
+import i18n  from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import translationEn from '../locales/eng/translation.json'
+import translationRu from '../locales/rus/translation.json'
+import translationUz from '../locales/uzb/translation.json'
+
+
+i18n.use(initReactI18next).init({
+    resources : {
+        en : {translation : translationEn},
+        ru : {translation : translationRu},
+        uz : {translation : translationUz}
+    },
+
+    lng : 'en',
+    fallbackLng : 'uz',
+    interpolation : {
+        escapevalue : false
+    },
+    returnObjects : true
+
+})
+
+export default i18n
